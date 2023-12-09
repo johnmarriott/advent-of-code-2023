af5d3e8a5537a998a1243268e88b5d3d89fe7132
John Marriott <john.marriott@campspot.com>
2023-12-09 06:29:14 -0600

    day 9 solution

---
+++ day09/extend.py
@@ -0,0 +1,46 @@
+import fileinput
+
+def sequence_differences(sequence):
+    """
+    return a sequence of differences of a sequence
+    e.g. [0 3 6 9 12] -> [3 3 3 3]
+    """
+
+    differences = []
+    for i in range(1, len(sequence)):
+        differences.append(sequence[i] - sequence[i - 1])
+    
+    return differences 
+
+def next_value_of_sequence(sequence):
+    differences = sequence_differences(sequence)
+
+    # base case - differences of this list is all zeros
+    if sum([abs(x) for x in differences]) == 0:
+        # sequence is constant so return any element
+        return sequence[0]
+    
+    # recursive case 
+    return next_value_of_sequence(differences) + sequence[-1]
+
+def previous_value_of_sequence(sequence):
+    differences = sequence_differences(sequence)
+
+    # base case - differences of this list is all zeros
+    if sum([abs(x) for x in differences]) == 0:
+        # sequence is constant so return any element
+        return sequence[0]
+    
+    # recursive case 
+    return sequence[0] - previous_value_of_sequence(differences)
+
+# read stdin to `field` with a padding of dots around the input
+next_values = []
+previous_values = []
+for line in fileinput.input():
+    sequence = [int(x) for x in line.strip().split()]
+    next_values.append(next_value_of_sequence(sequence))
+    previous_values.append(previous_value_of_sequence(sequence))
+    
+print(f"part one: {sum(next_values)}")
+print(f"part two: {sum(previous_values)}")
